63e1f053b770a838e7ac86d11a9293d78391ab9d
Thibaut Gauvin <gauvin.thibaut83@gmail.com>
2017-07-10 01:28:12 +0200

    Update traefik configuration & map host to service

--- docs/install.md
+++ docs/install.md
@@ -19,3 +19,12 @@ To provision virtual machines you need :
 - **Start & Build VM :**
     
         $ make provision
+
+
+- **Set hosts file :**
+
+        sudo nano /etc/hosts
+
+        # Add following entries
+        127.0.0.1 tick-stack.dev catalog.tick-stack.dev booking.tick-stack.dev billing.tick-stack.dev mongo.tick-stack.dev
+        192.168.42.100 tick-stack.com catalog.tick-stack.com booking.tick-stack.com billing.tick-stack.com mongo.tick-stack.com visualizer.tick-stack.com
--- src/CatalogService/config/routes.js
+++ src/CatalogService/config/routes.js
@@ -3,12 +3,18 @@
 module.exports = function(app) {
     var catalogCtrl = require('../controllers/catalogController');
 
-    app.route('/catalogs')
+    app.route('/')
+        .get(function(req, res) {
+            res.send("Welcome to the Catalog MicroService");
+        })
+    ;
+
+    app.route('/catalog')
         .get(catalogCtrl.list)
         .post(catalogCtrl.create)
     ;
 
-    app.route('/catalogs/:id')
+    app.route('/catalog/:id')
         .get(catalogCtrl.read)
         .put(catalogCtrl.update)
         .delete(catalogCtrl.delete)
